ed4be403c6a80a83e8f6b8bbd5a3ce94012b0219
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2025-03-21 11:09:47 +0100

    New formattings for different usecases

--- src/main/java/de/cuzim1tigaaa/colorlib/ColorLib.java
+++ src/main/java/de/cuzim1tigaaa/colorlib/ColorLib.java
@@ -1,6 +1,7 @@
 package de.cuzim1tigaaa.colorlib;
 
 import de.cuzim1tigaaa.colorlib.gradients.Gradient;
+import de.cuzim1tigaaa.colorlib.gradients.SingleColor;
 import net.md_5.bungee.api.ChatColor;
 
 import java.util.Set;
@@ -8,7 +9,12 @@ import java.util.Set;
 public class ColorLib {
 
 	public static String format(String message) {
-		return ChatColor.translateAlternateColorCodes('&', message);
+		Gradient singleColor = new SingleColor();
+		return ChatColor.translateAlternateColorCodes('&', singleColor.translate(message));
+	}
+
+	public static String format(String message, Gradient gradient) {
+		return gradient.translate(format(message));
 	}
 
 	public static String format(String message, Object... args) {
@@ -18,7 +24,10 @@ public class ColorLib {
 	public static String format(String message, Set<Gradient> gradient, Object... args) {
 		for(Gradient g : gradient)
 			message = g.translate(message);
-
 		return format(message, args);
+	}
+
+	public static String format(String message, Gradient gradient, Object... args) {
+		return format(message, Set.of(gradient), args);
 	}
 }
